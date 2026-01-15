9e546a7bd8c42b6cca2f47a4ee59bbb5e7cb390e
indextree <brilliantepoch22@gmail.com>
2026-01-16 00:05:31 +0900

    feat: fix watchRandomSeed.js and test 3, 4 and confirmed simulation

--- scripts/watchRandomSeed.js
+++ scripts/watchRandomSeed.js
@@ -8,6 +8,7 @@ const os = require('os');
 const fs = require('fs');
 
 const web3 = new Web3('http://localhost:8541');
+const BN = web3.utils.BN;
 
 const checkIntervalMS = 2539; // should be less than block time
 
@@ -15,55 +16,24 @@ const node1Path = '../data/node1/';
 const checkLogFileName = path.join(__dirname, `${node1Path}/checkRandomSeed.log`);
 const checkDebugFileName = path.join(__dirname, `${node1Path}/checkRandomSeedDebug.log`);
 fs.writeFileSync(checkLogFileName, '', 'utf8');
+fs.writeFileSync(checkDebugFileName, '', 'utf8');
 
 const RandomAuRa = require('../utils/getContract')('RandomAuRa', web3).instance;
 const ValidatorSetAuRa = require('../utils/getContract')('ValidatorSetAuRa', web3).instance;
 let collectRoundLengthBN;
+let commitPhaseLengthBN;
 let prevBlock;
 
-let seedState = (function () {
-    let lastSeedBN;
-    let lastChangeStart = 0;
-    return {
-        update: function (blockN, currentSeedBN, validatorsLength) {
-            let err_reason;
-            if (!lastSeedBN || lastSeedBN.isZero()) {
-                // not initialized yet
-                if (!currentSeedBN.isZero()) {
-                    // we start first round here
-                    lastChangeStart = blockN;
-                }
-            }
-            else {
-                if (blockN - lastChangeStart < validatorsLength) {
-                    // validators reveal their shares, so seed should change every block
-                    if (currentSeedBN.eq(lastSeedBN)) {
-                        err_reason = `seed didn't change in this block, seed value: ${currentSeedBN}, collectRoundLengthBN = ${collectRoundLengthBN}`;
-                    }
-                }
-                else if (blockN - lastChangeStart >= validatorsLength && collectRoundLengthBN.gt(new web3.utils.BN(blockN - lastChangeStart))) {
-                    // we are outside of revealing phase but new round has not yet started, so seed should not change
-                    if (!currentSeedBN.eq(lastSeedBN)) {
-                        err_reason = `seed changed outside of revealing phase, previous value: ${lastSeedBN}, current value: ${currentSeedBN}, collectRoundLengthBN = ${collectRoundLengthBN}, lastChangeStart = ${lastChangeStart}`;
-                    }
-                }
-                else if (collectRoundLengthBN.lte(new web3.utils.BN(blockN - lastChangeStart))) {
-                    // new round should start, so seed should change
-                    if (currentSeedBN.eq(lastSeedBN)) {
-                        err_reason = `seed didn't change in the beginning of a new round, seed value: ${currentSeedBN}, collectRoundLengthBN = ${collectRoundLengthBN}, lastChangeStart = ${lastChangeStart}`;
-                    }
-                    lastChangeStart = blockN;
-                }
-
-            }
-            lastSeedBN = currentSeedBN;
-            return {
-                err: !!err_reason,
-                reason: err_reason
-            };
-        }
-    }
-})();
+// Track seed changes per collection round using the same round math as RandomAuRa:
+// currentCollectRound = (blockNumber - 1) / collectRoundLength
+// commit phase length  = collectRoundLength / 2
+// reveal phase starts  = roundStart + commitPhaseLength
+let lastSeedBN;
+let trackedRoundStartBN;
+let trackedRevealStartBN;
+let trackedRoundEndBN;
+let seedChangedInReveal = false;
+let seenRevealBlock = false;
 
 // utility functions:
 function appendLine(str) {
@@ -78,6 +48,15 @@ async function wait(ms) {
     await new Promise(r => setTimeout(r, ms));
 }
 
+function calcRoundBounds(blockNumberBN) {
+    // roundStart = floor((block-1)/L)*L + 1
+    const roundIndexBN = blockNumberBN.subn(1).div(collectRoundLengthBN);
+    const roundStartBN = roundIndexBN.mul(collectRoundLengthBN).addn(1);
+    const revealStartBN = roundStartBN.add(commitPhaseLengthBN);
+    const roundEndBN = roundStartBN.add(collectRoundLengthBN).subn(1);
+    return { roundStartBN, revealStartBN, roundEndBN };
+}
+
 function doCheck() {
     Promise.all([
         web3.eth.getBlock('latest', false),
@@ -87,13 +66,53 @@ function doCheck() {
         let block = results[0];
         if (block.number == prevBlock) return;
         prevBlock = block.number;
-        let seed = new web3.utils.BN(results[1]);
-        let validatorsLength = results[2].length;
-        let report = seedState.update(block.number, seed, validatorsLength);
-        appendDebug(`[${block.number}]: seed=${seed} author=${block.author} validators=${results[2].join(',')} report=${report.reason||''}`);
-        if (report.err) {
-            appendLine(`[${block.number}]: report: ${report.reason}`);
+        const blockNumberBN = new BN(block.number.toString());
+        const seed = new BN(results[1]);
+        const validators = results[2];
+
+        const { roundStartBN, revealStartBN, roundEndBN } = calcRoundBounds(blockNumberBN);
+
+        // Detect round transition and validate previous round
+        if (!trackedRoundStartBN || !roundStartBN.eq(trackedRoundStartBN)) {
+            if (trackedRoundStartBN && seenRevealBlock && !seedChangedInReveal) {
+                appendLine(
+                    `[${trackedRoundEndBN.toString()}]: report: seed didn't change during reveal phase ` +
+                    `(roundStart=${trackedRoundStartBN.toString()}, revealStart=${trackedRevealStartBN.toString()}, roundEnd=${trackedRoundEndBN.toString()})`
+                );
+            }
+            trackedRoundStartBN = roundStartBN;
+            trackedRevealStartBN = revealStartBN;
+            trackedRoundEndBN = roundEndBN;
+            seedChangedInReveal = false;
+            seenRevealBlock = false;
         }
+
+        const isRevealPhase = blockNumberBN.gte(trackedRevealStartBN) && blockNumberBN.lte(trackedRoundEndBN);
+        if (isRevealPhase) {
+            seenRevealBlock = true;
+        }
+
+        let reportReason = '';
+        if (lastSeedBN && !seed.eq(lastSeedBN)) {
+            if (blockNumberBN.lt(trackedRevealStartBN)) {
+                reportReason =
+                    `seed changed during commit phase, previous value: ${lastSeedBN}, current value: ${seed}, ` +
+                    `roundStart=${trackedRoundStartBN.toString()}, revealStart=${trackedRevealStartBN.toString()}, roundEnd=${trackedRoundEndBN.toString()}`;
+                appendLine(`[${block.number}]: report: ${reportReason}`);
+            } else {
+                seedChangedInReveal = true;
+            }
+        }
+
+        const author = block.author || block.miner;
+        appendDebug(
+            `[${block.number}]: seed=${seed} author=${author} ` +
+            `validators=${validators.join(',')} ` +
+            `roundStart=${trackedRoundStartBN.toString()} revealStart=${trackedRevealStartBN.toString()} roundEnd=${trackedRoundEndBN.toString()} ` +
+            `phase=${isRevealPhase ? 'reveal' : 'commit'} report=${reportReason}`
+        );
+
+        lastSeedBN = seed;
     }).catch(e => {
         appendLine(`exception occured: ${e}`);
     });
@@ -101,16 +120,22 @@ function doCheck() {
 
 
 async function main() {
-    // initially wait until collectRoundLength is defined
+    // initially wait until round parameters are initialized
     while (true) {
-        let _collectRoundLengthBN = await RandomAuRa.methods.collectRoundLength().call();
-        if (_collectRoundLengthBN) {
-            collectRoundLengthBN = new web3.utils.BN(_collectRoundLengthBN);
-            break;
+        try {
+            const [_crl, _cpl] = await Promise.all([
+                RandomAuRa.methods.collectRoundLength().call(),
+                RandomAuRa.methods.commitPhaseLength().call(),
+            ]);
+            if (_crl && _crl !== '0' && _cpl && _cpl !== '0') {
+                collectRoundLengthBN = new BN(_crl);
+                commitPhaseLengthBN = new BN(_cpl);
+                break;
+            }
+        } catch (e) {
+            // ignore until RPC/contract is ready
         }
-        else {
-            await wait(checkIntervalMS);
-        }
+        await wait(checkIntervalMS);
     }
 
     setInterval(doCheck, checkIntervalMS);
